659c31a490c79880b19d5a063856c1d4a577c6e7
jlsajfj <jlsajfj@hotmail.com>
2017-03-21 22:47:02 -0300

    COMMENTING

--- UniversalArray.java
+++ UniversalArray.java
@@ -2,7 +2,11 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 /**
- * Write a description of class UniversalArray here.
+ * Universal array is a single array for all your array needs.<br />
+ * Need a string and an int in one array? no problem.<br />
+ * Bool and double? easy.<br />
+ * With Universal array, all types can be saved in one spot.<br />
+ * Whats the use for this I hear you say? I have 0 clue.
  * 
  * @author Joey Ma 
  * @version March 2017
@@ -13,10 +17,14 @@ public class UniversalArray
     private ArrayList<String> strings;
     private ArrayList<Boolean> bools;
     private ArrayList<Double> doubles;
+    /**
+     * Regex string, in format of # then a number indicating index in array then a char indicating the class type (integer,string,boolean,double) then an @ followed by the index the the corresponding arraylist
+     */
     private String loc;
     private int elements;
     /**
-     * Create a generic universal array with 0 elements
+     * Create a generic universal array with 0 elements.<br />
+     * Initializes all the internal ArrayLists and creates the regex string to search.<br />
      */
     public UniversalArray(){
         ints = new ArrayList<Integer>();
@@ -27,6 +35,12 @@ public class UniversalArray
         elements=0;
     }
     
+    /**
+     * Add an element to the Array<br />
+     * Generics was used so that instead of added a new overloaded function when a new class is to be added,<br />
+     * just add another if statement to the end.
+     * @params e the element to be added (any type)
+     */
     public <E> void add(E e){
         loc+="#"+elements;
         if(e instanceof Integer){
@@ -48,11 +62,18 @@ public class UniversalArray
         elements++;
     }
     
+    /**
+     * Just a testing class that outputs the current regex string.
+     */
     public void test(){
         System.out.println(loc);
     }
     
-    @SuppressWarnings("unchecked")
+    /**
+     * Getter method to get the element at index i regardless of class
+     * @param i index of element to be retrieved
+     * @return Object a generic object representing whatever was started with
+     */
     public <E> E get(int i){
         Pattern pattern = Pattern.compile("#"+i+"[^0-9]");
         Matcher match = pattern.matcher(loc);
@@ -63,7 +84,7 @@ public class UniversalArray
             Matcher found = nxt.matcher(loc);
             found.find(match.end());
             System.out.println(loc.substring(found.start()+1,found.end()-1));
-            System.out.println(strings.get(Integer.parseInt(loc.substring(found.start()+1,found.end()-1))));
+            //System.out.println(strings.get(Integer.parseInt(loc.substring(found.start()+1,found.end()-1))));
             switch(loc.charAt(match.end()-1)){
                 case 'i':
                 return (E) ints.get(Integer.parseInt(loc.substring(found.start()+1,found.end()-1)));
